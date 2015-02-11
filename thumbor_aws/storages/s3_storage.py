effaa7fcfb7b94208282fdb62791a051a5f16280
Lex Toumbourou <lextoumbourou@gmail.com>
2015-02-11 18:04:51 +1100

    Store files in the same fashion as Thumbor storage.

--- thumbor_aws/storages/s3_storage.py
+++ thumbor_aws/storages/s3_storage.py
@@ -126,7 +126,7 @@ class Storage(BaseStorage):
 
     def normalize_path(self, path):
         digest = hashlib.sha1(path.encode('utf-8')).hexdigest()
-        return "thumbor/storage/"+digest
+        return "thumbor/storage/{}/{}".format(digest[:2], digest[2:])
 
     def is_expired(self, key):
         if key:
